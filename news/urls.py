2fe508250efb9e51e752688f0a656b3bcd55ae95
mahamudh472 <mahadymahamudh472@gmail.com>
2025-07-24 21:49:42 +0600

    24 jul update

--- news/urls.py
+++ news/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'news'
 
 urlpatterns = [
-    path('', views.home, name="news_home"),
+    path('', views.home, name="news_feed"),
     path('create', views.create_news, name="create_news"),
     path('details', views.news_details, name="news_details")
 ]
