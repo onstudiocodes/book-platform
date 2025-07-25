af3b648f9d173a1790fdea8e4a1bead964a56a53
mahamudh472 <mahadymahamudh472@gmail.com>
2025-07-25 21:45:22 +0600

    25 july update

--- news/views.py
+++ news/views.py
@@ -1,11 +1,27 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from author.forms import NewsForm, NewsImageFormSet
 # Create your views here.
 def home(request):
     return render(request, 'news/index.html')
 
 def create_news(request):
-    return render(request, 'news/create_news.html')
+    if request.method == 'POST':
+        form = NewsForm(request.POST)
+        formset = NewsImageFormSet(request.POST, request.FILES, instance=form.instance)
+        
+        if form.is_valid() and formset.is_valid():
+            news = form.save()
+            formset.instance = news
+            formset.save()
+            return redirect('news:news_feed')
+    else:
+        form = NewsForm()
+        formset = NewsImageFormSet()
+    
+    return render(request, 'news/create_news.html', {
+        'form': form,
+        'formset': formset
+    })
 
 def news_details(request):
     return render(request, 'news/news_details.html')
